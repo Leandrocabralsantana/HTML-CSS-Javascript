24ba358472d24133b0f1829b33d2ec731c2d556b
Leandrocabralsantana <leomattsantana@gmail.com>
2022-04-19 23:46:45 -0300

    BaSP-2022:Javascript before sending

--- Semana-05/exercises/04_if_else.js
+++ Semana-05/exercises/04_if_else.js
@@ -1,10 +1,19 @@
-If Else
-Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”.
-Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+/*4)If Else
+A)Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”.*/
+
+console.log('Ejercicio-4-A');
+var edad = Math.floor(Math.random() * 100);
+console.log(edad
+    if (edad >=0,5 = 'Greater than 0,5')
+    else ('Lower than 0,5')
+
+/*B)Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
 “Bebe” si la edad es menor a 2 años;
 “Niño” si la edad es entre 2 y 12 años;
 “Adolescente” entre 13 y 19 años;
 “Joven” entre 20 y 30 años;
 “Adulto” entre 31 y 60 años;
 “Adulto mayor” entre 61 y 75 años;
-“Anciano” si es mayor a 75 años.
+“Anciano” si es mayor a 75 años.*/
+
+console.log('Ejercicio-4-B');
