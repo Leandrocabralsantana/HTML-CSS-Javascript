69aa74e671882b68d68a1c3c4d9bdaf80083849a
Leandrocabralsantana <leomattsantana@gmail.com>
2022-04-19 22:46:29 -0300

    BaSP-2022:Almost finished Strings exercises

--- Semana-05/exercises/02_strings.js
+++ Semana-05/exercises/02_strings.js
@@ -19,3 +19,26 @@ var palabraLarga;
 palabraLarga = "Paralelepipedo";
 var palabraSubLarga = palabraLarga.substring(palabraLarga.length-3);
 console.log(palabraSubLarga);
+
+/*D)Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).*/
+console.log('Ejercicio-2-D');
+var palabraLarga;
+palabraLarga = "Paralelepipedo";
+var palabraSubLarga = palabraLarga.substring(0,1).toUpperCase() + palabraLarga.substring(1, 14).toLowerCase();
+console.log(palabraSubLarga);
+
+/*E)Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).*/
+console.log('Ejercicio-2-E');
+var palabraEspaciada;
+palabraEspaciada = "Paralelepipedo Uno";
+var palabraSubLarga = palabraEspaciada.indexOf(' ', 0)
+console.log(palabraSubLarga);
+
+/*F)Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).*/
+console.log('Ejercicio-2-F');
+var palabraLarga;
+palabraLarga = "Paralelepipedo Internet";
+var palabraEspaciada = palabraLarga.indexOf('
+console.log(palabraEspaciada)
+var palabraSubLarga = palabraLarga.substring(0,1).toUpperCase() + palabraLarga.substring(1, 24).toLowerCase();
+console.log(palabraSubLarga);
---
+++ Semana-05/exercises/03_arrays.js
@@ -0,0 +1,15 @@
+/*3)Arrays
+A)Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log).*/
+
+
+/*B)Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).*/
+
+/*C)Agregar un elemento al principio y al final del array (utilizar unshift y push).*/
+
+/*D)Quitar un elemento del principio y del final del array (utilizar shift y pop).*/
+
+/*E)Invertir el orden del array (utilizar reverse).*/
+
+/*F)Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).*/
+
+/*G)Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).*/
