12736bb803b1e9870d95d6c4d00f4fd0538e2bc7
Leandrocabralsantana <leomattsantana@gmail.com>
2022-04-19 23:40:10 -0300

    BaSP-2022:Arrays finished

--- Semana-05/exercises/03_arrays.js
+++ Semana-05/exercises/03_arrays.js
@@ -1,15 +1,33 @@
 /*3)Arrays
 A)Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log).*/
-
+console.log('Ejercicio-3-A');
+var mesesArray =  ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+console.log(mesesArray[4] + mesesArray[10]);
 
 /*B)Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).*/
+console.log('Ejercicio-3-B');
+console.log(mesesArray.sort())
 
 /*C)Agregar un elemento al principio y al final del array (utilizar unshift y push).*/
+console.log('Ejercicio-3-C');
+mesesArray.unshift('MesCero') + mesesArray.push('Fin');
+console.log(mesesArray);
 
 /*D)Quitar un elemento del principio y del final del array (utilizar shift y pop).*/
+console.log('Ejercicio-3-D');
+mesesArray.shift();
+mesesArray.pop();
+console.log(mesesArray);
 
 /*E)Invertir el orden del array (utilizar reverse).*/
+console.log('Ejercicio-3-E');
+console.log(mesesArray.reverse());
 
 /*F)Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).*/
+console.log('Ejercicio-3-F');
+console.log(mesesArray.join('-'));
 
 /*G)Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).*/
+console.log('Ejercicio-3-G');
+var mesesArrayCorto = mesesArray.slice(4,10);
+console.log(mesesArrayCorto);
---
+++ Semana-05/exercises/04_if_else.js
@@ -0,0 +1,10 @@
+If Else
+Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”.
+Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+“Bebe” si la edad es menor a 2 años;
+“Niño” si la edad es entre 2 y 12 años;
+“Adolescente” entre 13 y 19 años;
+“Joven” entre 20 y 30 años;
+“Adulto” entre 31 y 60 años;
+“Adulto mayor” entre 61 y 75 años;
+“Anciano” si es mayor a 75 años.
